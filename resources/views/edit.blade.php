14d12953955d293047ad5e6dfa9f7dc586d854c7
Peerapat Sawettraporn <macnum4368@gmail.com>
2022-02-23 02:09:46 +0700

    Edited

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -4,7 +4,22 @@
         {{ __('Edit Task') }}
     </h2>
 </x-slot>
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<style>
+    .edit{
+            background-color: white;
+            color: black;
+            border: 2px solid #B696FA;
+        }
+</style>
+<body>
 <div class="py-12">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-5">
@@ -19,11 +34,14 @@
                 </div>
 
                 <div class="form-group">
-                    <button type="submit" name="update" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Update task</button>
+                    <button type="submit" name="update" class="edit">Update task</button>
                 </div>
             {{ csrf_field() }}
             </form>
         </div>
     </div>
 </div>
+</body>
+</html>
+
 </x-app-layout>
