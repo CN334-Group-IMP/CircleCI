eb6f64c51caa864da2ad3ea8de820da306ceb64d
Suyada <ippyzuza@gmail.com>
2022-03-16 09:24:52 +0700

    test

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -11,8 +11,20 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
+
+    public function testBasicTest()
     {
         $this->assertTrue(true);
+    }
+
+    public function testUserCreation()
+    {
+        $user = new User([
+            'name' => "Test User",
+            'email' => "test@mail.com",
+            'password' => bcrypt("testpassword")
+        ]);   
+
+        $this->assertEquals('Test User', $user->name);
     }
 }
