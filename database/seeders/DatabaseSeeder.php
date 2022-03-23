501e4fc04434c76659f424737268aa1c2d007674
Suyada <ippyzuza@gmail.com>
2022-03-23 10:18:46 +0700

    add seeder for real

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +15,15 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        DB::table('tasks')->insert([
+            'task name' => 'task1',
+            'task description' => 'task1 test',
+        ]);
+
+        DB::table('tasks')->insert([
+            'task name' => 'task2',
+            'task description' => 'task2 test',
+        ]);
     }
 }
